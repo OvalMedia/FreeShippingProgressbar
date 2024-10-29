d2274004222631556da1988ba2d82a5f733c82e3
Ole Hornauer <oh@oval-media.de>
2024-10-29 16:38:01 +0100

    Code cleanup

--- src/ViewModel/Cart.php
+++ src/ViewModel/Cart.php
@@ -1,29 +1,32 @@
 <?php
 declare(strict_types=1);
+
 namespace OM\FreeShippingProgressBar\ViewModel;
 
-use \Magento\Store\Model\ScopeInterface;
-use \Magento\Sales\Model\Order\Shipment;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Sales\Model\Order\Shipment;
+use OM\FreeShippingProgressBar\Service\Config;
+use OM\FreeShippingProgressBar\Service\Data;
 
 class Cart implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     /**
      * @var \OM\FreeShippingProgressBar\Service\Config
      */
-    protected \OM\FreeShippingProgressBar\Service\Config $_config;
+    protected Config $_config;
 
     /**
      * @var \OM\FreeShippingProgressBar\Service\Data
      */
-    protected \OM\FreeShippingProgressBar\Service\Data $_data;
+    protected Data $_data;
 
     /**
      * @param \OM\FreeShippingProgressBar\Service\Config $config
      * @param \OM\FreeShippingProgressBar\Service\Data $data
      */
     public function __construct(
-        \OM\FreeShippingProgressBar\Service\Config $config,
-        \OM\FreeShippingProgressBar\Service\Data $data
+        Config $config,
+        Data $data
     ) {
         $this->_config = $config;
         $this->_data = $data;
